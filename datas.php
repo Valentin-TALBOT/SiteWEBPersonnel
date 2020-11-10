8beb4ec41841e457151683534d5fbf48efb60971
Valentin Talbot <vtalbot@sio.local>
2020-11-10 10:09:57 +0100

    Commit n°1: Rajout pages en PHP, rajout d'un dossier Assets contenant les photos et modification des pages HTML.

---
+++ datas.php
@@ -0,0 +1,9 @@
+<?php
+$menu=['A propos','Compétences','Expérience','Formation','Contact']
+
+$competences=[['partie'=>'Développement, technologies','subTitle'=>'Sous titre de la partie','icon'=>'code','competences'=>
+                   [
+                     ['title'=>'A l\'aise ++','niveau'=>100,'items'=>['HTML/CSS','Javascript'],
+                     ['title'=>'A l\'aise +','niveau'=>60,'items'=>['Python']              
+                   ]
+             ]];
