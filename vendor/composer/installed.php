f2cfcf6d81a3a22e3d3c52f6a99fb0b1d20359d0
Valentin <vtalbot2002@gmail.com>
2020-11-24 12:53:14 +0100

    Ajour de composer.json, composer.lock et du dossier vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c8c0622af52e7a34f06e60a513f79aec009bf383',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c8c0622af52e7a34f06e60a513f79aec009bf383',
+    ),
+  ),
+);
